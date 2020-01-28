ca5a5261e1492f840e63098db47126d301527226
Christian Ortiz <cortiz@baufest.com>
2020-01-28 14:55:00 -0300

    UI de app

--- test/src/App.js
+++ test/src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Slider from './components/Slider'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
@@ -13,7 +21,42 @@ function App() {
         
       </header>
       <div className="general-container">
-        <Slider></Slider>
+          <Card >
+            <CardContent className="item-list">
+              <div className="container-img-carousel">
+                  <FontAwesomeIcon className="heart-liked" icon={faHeart}></FontAwesomeIcon>
+                <Slider></Slider>
+              </div>
+              <div className="container-info">
+                <Typography className="title-item">Si vas a utilizar un pasaje de Lorem Ipsum, necesitás esta izar un pasaje de Lorem Ipsum, necesitás esta</Typography>
+                <div className="ubication-title">
+                  <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                  <p>Juan Francisco Seguí 3900, Palermo Chico, Palermo</p>
+                </div>
+                <Typography className="description">
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de
+texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el
+año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)
+                </Typography>
+                <div className="price-info">
+                  <Typography className="price">U$S 1.400.000</Typography>
+                  <Typography className="sub-price">$/m23.680</Typography>
+                </div>
+                <CardActions className="footer-item">
+                  <div className="amenities-content">
+                    <ul>
+                      <li>380 m2</li>
+                      <li>3 Dormitorios</li>
+                      <li>2 Baños</li>
+                      <li>2 Cocheras</li>
+                    </ul>
+                  </div>
+                  <Button size="small">CONTACTAR</Button>
+                </CardActions>
+              </div>
+              
+            </CardContent>
+          </Card>
       </div>
     </div>
   );
--- test/src/components/Slider/index.js
+++ test/src/components/Slider/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Slider from '@farbenmeer/react-spring-slider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from'@fortawesome/free-solid-svg-icons';
+import HotelImg from '../../assets/img/hotel_1.jpg';
+import HotelImg2 from '../../assets/img/hotel_2.jpg';
+
 
 class SliderComponent extends React.Component {
     constructor(props) {
-      super(props);
+      super(props);   
     }
+
     componentDidMount() {}
   
     HandleValidateTerms() {
@@ -14,38 +21,43 @@ class SliderComponent extends React.Component {
         console.log(`changed to slide ${index}`)  
     }
 
-    //https://reactjsexample.com/a-slider-which-uses-react-spring-under-the-hood/
-
 	ArrowComponent({onClick, direction}){
 		return (
 			<div
 				style={{
-					border: '1px solid black',
-					padding: '1em',
-					backgroundColor: 'red'
+          padding: '1em',
+          cursor: 'pointer',
+          marginTop: -20
 				}}
 				onClick={onClick}
 			>
-				{direction}
+        {direction == 'left' ? (
+          <FontAwesomeIcon className="arrow-slider" icon={faChevronLeft}></FontAwesomeIcon>
+        ):(
+          <FontAwesomeIcon className="arrow-slider" icon={faChevronRight}></FontAwesomeIcon>
+        )}
+				
 			</div>
 		);
-	};
+  };
+
+  
   
     render() {
       return (
         <Slider 
-            activeIndex={1}  
-            hasBullets 
-            BulletComponent={this.BulletComponent}
+            activeIndex={2}  
+            hasArrows
             ArrowComponent={this.ArrowComponent}
             onSlideChange={this.onSlideChange}
             >
-                <div>child 1</div>
-                <div>child 2</div>
-                <div>child 3</div>
+            <img src={HotelImg} alt="asdas" />
+            <img src={HotelImg2} alt="asdas" />
         </Slider>
       );
     }
+ 
   }
+
   
   export default SliderComponent;
